85017647faa74eaaf15fdcdfb9b774b7d5afda98
Mert Çalkan <calkanmert@outlook.com>
2023-11-16 23:43:15 +0300

    fix: ci

--- infrastructure/ci.sh
+++ infrastructure/ci.sh
@@ -1,9 +1,8 @@
-application_image_path=../application
+application_image_path=$GITHUB_WORKSPACE/application
 
 function build_and_deploy() {
-  cd $application_image_path
   docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
-  docker build -t $DOCKER_HUB_REPO:latest .
+  docker build -t $DOCKER_HUB_REPO:latest $application_image_path
   docker push $DOCKER_HUB_REPO:latest
 }
 
